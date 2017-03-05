8581b4a5307f1fac12f2b862b45a46cd8932bad3
Ariana Clark <ariana.clark@gmail.com>
2017-03-05 15:01:54 -0700

    setTimeout recursion not returning promise

--- server/helpers/order-helper.js
+++ server/helpers/order-helper.js
@@ -1,17 +1,36 @@
 'use strict'
 var exports = module.exports = {}
+const express = require( 'express' );
+const oa = require( './auth-helper' ).oa
+const querystring = require( 'querystring' )
+const Item = require( './order-helper' ).Item
+
+const token =
 
 
-exports.Order = class Order {
-  constructor( receipt_id, creation_tsz, name, message_from_buyer, total_price ) {
-    this.orderId = receipt_id
-    this.orderDate = new Date( creation_tsz )
-    this.buyerName = name
-    this.buyerMessage = message_from_buyer
-    this.totalPrice = total_price
-    this.items = []
+  exports.Order = class Order {
+    constructor( receipt_id, creation_tsz, name, message_from_buyer, total_price ) {
+      this.orderId = receipt_id
+      this.orderDate = new Date( creation_tsz )
+      this.buyerName = name
+      this.buyerMessage = message_from_buyer
+      this.totalPrice = total_price
+      this.items = []
+    }
+
+    addItem( item ) {
+      this.items.push( item )
+    }
+    get shipBy() {
+      let maxProcessing = 0
+      for ( var i = 0; i < this.items.length; i++ ) {
+        if ( this.items[ i ].processingTime > maxProcessing ) {
+          maxProcessing = this.items[ i ].processingTime
+        }
+      }
+      return new Date( this.orderDate.getFullYear(), this.orderDate.getMonth, ( this.orderDate.getDate() + maxProcessing ) )
+    }
   }
-}
 
 exports.Item = class Item {
   constructor( transaction_id, listing_id, receipt_id, title, quantity, price, variations, url ) {
@@ -36,5 +55,103 @@ exports.Item = class Item {
       this.variationName2 = variations[ 1 ].formatted_name
       this.variationValue2 = variations[ 1 ].formatted_value
     }
+    this.imageThumbnailUrl = ''
+    this.imageFullUrl = ''
+    this.processingTime = 0
+  }
+
+}
+
+// exports.getReceipts = new Promise( ( resolve, reject ) => {
+//   oa.getProtectedResource( //get open receipts
+//     "https://openapi.etsy.com/v2" + `/shops/${shop_id}/receipts/open`,
+//     "GET",
+//     req.session.oauth.access_token,
+//     req.session.oauth.access_token_secret,
+//     function( error, data, response ) {
+//       if ( error ) {
+//         console.log( error );
+//         return reject( error )
+//       } else {
+//         console.log( "I did it!" );
+//         JSON.parse( data ).results.forEach( function( receipt ) {
+//           let order = new Order( receipt.receipt_id, receipt.creation_tsz, receipt.name, receipt.message_from_buyer, receipt.total_price )
+//           orders.push( order )
+//         } )
+//         console.log( 'orders' );
+//         resolve( orders )
+//       }
+//     }
+//   )
+// } )
+//
+// exports.getTransactionItems = new Promise( ( resolve, reject ) => {
+//
+//   oa.getProtectedResource( //get transaction items
+//     "https://openapi.etsy.com/v2" + `/shops/${shop_id}/transactions?` + querystring.stringify( {
+//       limit: 100
+//     } ),
+//     "GET",
+//     token,
+//     token_secret,
+//     function( error, data, response ) {
+//       if ( error ) {
+//         console.log( error );
+//         return reject( error )
+//       } else {
+//         var listingIdArr = []
+//         var imageListingIdArr = []
+//         var itemsTrxData = []
+//         // console.log( JSON.parse( data ) );
+//         // res.send( JSON.parse( data ) )
+//         let rawItemsArr = JSON.parse( data ).results
+//
+//         rawItemsArr.forEach( function( transaction ) {
+//           if ( transaction.shipped_tsz !== null ) {
+//
+//             listingIdArr.push( transaction.listing_id )
+//
+//             imageListingIdArr.push( transaction.image_listing_id )
+//
+//             let item = new Item( transaction.transaction_id, transaction.listing_id, transaction.receipt_id, transaction.title, transaction.quantity, transaction.price, transaction.variations, transaction.url )
+//
+//             itemsTrxData.push( item )
+//           }
+//
+//         } )
+//         listingIdArr = [ ...new Set( listingIdArr ) ];
+//         imageListingIdArr = [ ...new Set( imageListingIdArr ) ];
+//         resolve( [ itemsTrxData, listingIdArr, imageListingIdArr ] )
+//       }
+//     }
+//   )
+// } )
+exports.timeoutQ = function timeoutQueue( array, callback, min = 0, max = 6 ) {
+  console.log( "recursion" );
+  let arrayChunk = array.slice( min, max )
+  if ( min > 11 ) {
+    return array
+  } else {
+    setTimeout( function() {
+      timeoutQueue( array, callback, min, max )
+    }, 1000 )
+    min += 6
+    max += 6
+    return callback( arrayChunk )
+  }
+}
+
+exports.timeoutListing = function timeoutListing( array, callback, min = 0, max = 6 ) {
+  console.log( "recursion" );
+  let arrayChunk = array.slice( min, max )
+  if ( min > 11 ) {
+    return array
+  } else {
+    setTimeout( function() {
+      timeoutQueue( array, callback, min, max )
+    }, 1000 )
+    min += 6
+    max += 6
+    return callback( arrayChunk )
   }
 }
--- server/routes/orders.js
+++ server/routes/orders.js
@@ -8,10 +8,11 @@ const axios = require( 'axios' )
 const querystring = require( 'querystring' )
 const Order = require( '../helpers/order-helper.js' ).Order
 const Item = require( '../helpers/order-helper.js' ).Item
+const timeoutQueue = require( '../helpers/order-helper.js' ).timeoutQ
+// const getReceipts = require( '../helpers/order-helper.js' ).getReceipts
+// const getTransactionItems = require( '../helpers/order-helper.js' ).getTransactionItems
 const oa = require( '../helpers/auth-helper.js' ).oa
 
-
-console.log( Order );
 
 
 // var key = '0g2b7qc51yenv3co98av1m5k';
@@ -58,12 +59,10 @@ router.get( '/all', function( req, res, next ) {
 // test routes TODO: Delete before production
 router.get( '/by_status', function( req, res, next ) {
   const shop_id = req.session.shop_id
-  let uri = `/shops/${shop_id}/receipts/open`
-  const paramString = '?' + querystring.stringify( {
-    limit: 75
-  } )
+  // let uri = `/shops/${shop_id}/receipts/open`
+
   oa.getProtectedResource(
-    "https://openapi.etsy.com/v2" + uri + paramString,
+    "https://openapi.etsy.com/v2" + `/shops/${shop_id}/receipts/open`,
     "GET",
     req.session.oauth.access_token,
     req.session.oauth.access_token_secret,
@@ -86,7 +85,7 @@ router.get( '/by_status', function( req, res, next ) {
 
 router.get( '/listings', function( req, res, next ) {
   const shop_id = req.session.shop_id
-  let uri = `/listings/275905818`
+  let uri = `/shops/${shop_id}/listings/`
   // const paramString = '?' + querystring.stringify( {
   //   listing_id: 275905818
   // } )
@@ -106,40 +105,185 @@ router.get( '/listings', function( req, res, next ) {
     }
   )
 } )
+
+
+// BEGIN ORDER BUILDING
 router.get( '/items', function( req, res, next ) {
   const shop_id = req.session.shop_id
-  let uri = `/shops/${shop_id}/transactions`
-  const paramString = '?' + querystring.stringify( {
-    limit: 75
-  } )
-  oa.getProtectedResource(
-    "https://openapi.etsy.com/v2" + uri + paramString,
-    "GET",
-    req.session.oauth.access_token,
-    req.session.oauth.access_token_secret,
-    function( error, data, response ) {
-      if ( error ) {
-        console.log( error );
-        res.send( error )
-      } else {
-        // console.log( JSON.parse( data ) );
-        // res.send( JSON.parse( data ) )
-        var rawItemsArr = JSON.parse( data ).results
-        var listingIdArr = []
-        var imageListingIdArr = []
-        var itemsTrxData = []
-        rawItemsArr.forEach( function( transaction ) {
-          listingIdArr.push( transaction.listing_id )
-          imageListingIdArr.push( transaction.image_listing_id )
-          let item = new Item( transaction.transaction_id, transaction.listing_id, transaction.receipt_id, transaction.title, transaction.quantity, transaction.price, transaction.variations, transaction.url )
-          itemsTrxData.push( item )
-        } )
+  var orders = []
+  var listingIdArr = []
+  var uniqueListingIdArr = []
+  var listings = []
+  var imageListingIdArr = []
+  var uniqueImageListingIdArr = []
+  var images = []
+  var itemsTrxData = []
+  let time = 100
 
-        res.send( itemsTrxData )
-      }
-    }
-  )
+
+
+  new Promise( ( resolve, reject ) => {
+      oa.getProtectedResource( //get transaction items
+        "https://openapi.etsy.com/v2" + `/shops/${shop_id}/transactions?` + querystring.stringify( {
+          limit: 100
+        } ),
+        "GET",
+        req.session.oauth.access_token,
+        req.session.oauth.access_token_secret,
+        function( error, data, response ) {
+          if ( error ) {
+            console.log( error );
+            return reject( error )
+          } else {
+            let rawItemsArr = JSON.parse( data ).results
+
+            rawItemsArr.forEach( function( transaction ) {
+              if ( transaction.shipped_tsz !== null ) {
+
+                listingIdArr.push( transaction.listing_id )
+
+                imageListingIdArr.push( [ transaction.listing_id, transaction.image_listing_id ] )
+
+                let item = new Item( transaction.transaction_id, transaction.listing_id, transaction.receipt_id, transaction.title, transaction.quantity, transaction.price, transaction.variations, transaction.url )
+
+                itemsTrxData.push( item )
+              }
+
+            } )
+            uniqueListingIdArr = [ ...new Set( listingIdArr ) ];
+            console.log( "unique_listings", uniqueListingIdArr.length );
+            uniqueImageListingIdArr = [ ...new Set( imageListingIdArr ) ];
+            resolve( [ itemsTrxData, uniqueListingIdArr, imageListingIdArr ] )
+          }
+        }
+      )
+    } )
+    .then( ( data ) => {
+      new Promise( ( resolve, reject ) => {
+        oa.getProtectedResource( //get open receipts
+          "https://openapi.etsy.com/v2" + `/shops/${shop_id}/receipts/open`,
+          "GET",
+          req.session.oauth.access_token,
+          req.session.oauth.access_token_secret,
+          function( error, data, response ) {
+            if ( error ) {
+              console.log( error );
+              return reject( error )
+            } else {
+              console.log( "I did it!" );
+              JSON.parse( data ).results.forEach( function( receipt ) {
+                let order = new Order( receipt.receipt_id, receipt.creation_tsz, receipt.name, receipt.message_from_buyer, receipt.total_price )
+                orders.push( order )
+              } )
+              console.log( 'orders' );
+              resolve( orders )
+            }
+          }
+        )
+      } ).then( ( data ) => {
+        let min = 0
+        let max = 6
+        let arrayChunk = uniqueListingIdArr.slice( min, max )
+        // var listingQueue = function( arrayChunk ) {
+        //   return Promise.all( arrayChunk.forEach( function( id ) {
+        //     return new Promise( ( resolve, reject ) => {
+        //       oa.getProtectedResource(
+        //         "https://openapi.etsy.com/v2" + `/listings/${id}`,
+        //         "GET",
+        //         req.session.oauth.access_token,
+        //         req.session.oauth.access_token_secret,
+        //         function( error, data, response ) {
+        //           if ( error ) {
+        //             console.log( error );
+        //             reject( error )
+        //           } else {
+        //             listings.push( JSON.parse( data ).results[ 0 ] )
+        //             console.log( "* * *" );
+        //             resolve( data )
+        //
+        //           }
+        //         } )
+        //     } )
+        //   } ) )
+        //
+        // }
+        return Promise.all( timeoutQueue( uniqueListingIdArr, function( arrayChunk = arrayChunk ) {
+          Promise.all(
+            arrayChunk.forEach( function( id ) {
+              return new Promise( ( resolve, reject ) => {
+                oa.getProtectedResource(
+                  "https://openapi.etsy.com/v2" + `/listings/${id}`,
+                  "GET",
+                  req.session.oauth.access_token,
+                  req.session.oauth.access_token_secret,
+                  function( error, data, response ) {
+                    console.log( arrayChunk );
+                    if ( error ) {
+                      console.log( error );
+                      return reject( error )
+                    } else {
+                      listings.push( JSON.parse( data ).results[ 0 ] )
+                      console.log( "* * *" );
+                      return resolve( data )
+
+                    }
+                  } )
+              } )
+            } ) )
+
+        } ) ).then( () => {
+          res.send( [ "listings", listings ] )
+
+        } )
+      } )
+    } )
+
+  // .then( ( data ) => {
+  //   let min = 0
+  //   let max = 8
+  //   let index = 0
+  //   while ( min <= uniqueImageListingIdArr.length ) {
+  //     let timer = setTimeout( function( min, max ) {
+  //       let arrayChunk = uniqueImageListingIdArr.slice( min, max )
+  //       Promise.all( arrayChunk.forEach( function( id ) {
+  //         new Promise( ( resolve, reject ) => oa.getProtectedResource(
+  //           "https://openapi.etsy.com/v2" + `/listings/${uniqueListingIdArr[index]}/images/${id}`,
+  //           "GET",
+  //           req.session.oauth.access_token,
+  //           req.session.oauth.access_token_secret,
+  //           function( error, data, response ) {
+  //             index++
+  //             if ( error ) {
+  //               console.log( error );
+  //               return reject( error )
+  //             } else {
+  //               // console.log( JSON.parse( data ) );
+  //               console.log( "listing" );
+  //               images.push( JSON.parse( data ) )
+  //               resolve( data )
+  //             }
+  //           } ) )
+  //       } ) )
+  //     }, 2000, min, max )
+  //     min += 10
+  //     max += 10
+  //     clearTimeout( timer )
+  //   }
+  // } )
+
+  // } )
+
 } )
+
+
+
+
+// Promise.all( [ getReceipts(), getTransactionItems() ] )
+//   .then( data => {
+//     console.log( 'stuff' );
+//   } )
+
+
 
 
 // function getProtectedEtsyResource( req, res, resourceURI, method ) {
