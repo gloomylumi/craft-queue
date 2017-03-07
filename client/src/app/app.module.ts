c5ae1927c827d1c08f4d90194890dc2449564895
Ariana Clark <ariana.clark@gmail.com>
2017-03-06 22:01:33 -0700

    added angular2-materialize

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import "materialize-css"
 import { MaterializeModule } from 'angular2-materialize';
 
 
