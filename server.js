bd1adf76c69ef89b499c8dde24eb2a6520c5bfad
Ariana Clark <ariana.clark@gmail.com>
2017-03-12 22:24:12 -0600

    updated cors middleware with pre-flighting

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const app = express();
 // app.use( cookieParser() )
 var cors = require( 'cors' )
 
-app.use( cors( {
-  credentials: true,
-  origin: true
-} ) )
+app.use( cors() )
 // app.use( function( req, res, next ) {
 //   res.header( "Access-Control-Allow-Origin", "*" );
 //   res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
@@ -72,6 +69,9 @@ if ( process.env.NODE_ENV !== 'production' ) {
   app.use( express.static( path.join( __dirname, 'dist' ) ) );
 }
 
+app.options( '*', cors( {
+  origin: true
+} ) );
 // API ROUTES
 const auth = require( './routes/auth' );
 app.use( '/api/auth', auth )
