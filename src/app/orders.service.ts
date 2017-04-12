f861fa0b74a60bd210fc357396434b598d9618dd
Ariana Clark <ariana.clark@gmail.com>
2017-04-11 20:17:04 -0600

    fixed sorting bug

--- src/app/orders.service.ts
+++ src/app/orders.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
+import { Order } from './order'
 
 import 'rxjs/add/operator/toPromise';
 
@@ -9,32 +10,48 @@ export class OrdersService {
 
   constructor(private http: Http) { }
 
-  getOrders(): any {
+  getOrders() {
     return this.http.get(this.ordersUrl)
       .toPromise()
       .then(response => {
-        let sorted = this.quickSortByShipDate(response.json())
+        console.log("response:", response.json())
+        let sorted: Array<Order> = response.json()[0].sort(this.compareDates)
         console.log(sorted)
         return sorted
       })
       .catch(this.handleError)
 
   }
-  quickSortByShipDate(list: any[]): any[] {
-    if (list.length == 0) {
-      return [];
+
+  compareDates(a: Order, b: Order): number {
+    console.log(a, b)
+    let dateA: number = a.shipBy.valueOf()
+    let dateB: number = b.shipBy.valueOf()
+    if (dateA < dateB) {
+      return -1
     }
-    var lesser = []; var greater = []; var pivot = list[0];
-    var pivotParam = list[0].shipBy;
-    for (var i = 1; i < list.length; i++) {
-      if (list[i].shipBy < pivotParam) {
-        lesser.push(list[i]);
-      } else {
-        greater.push(list[i]);
-      }
+    if (dateA > dateB) {
+      return 1
     }
-    return this.quickSortByShipDate(lesser).concat(pivot, this.quickSortByShipDate(greater));
+    return 0
+
   }
+
+  // quickSortByShipDate(list: any[]): any[] {
+  //   if (list.length == 0) {
+  //     return [];
+  //   }
+  //   var lesser = []; var greater = []; var pivot = list[0];
+  //   var pivotParam = Date.UTC(list[0].shipBy);
+  //   for (var i = 1; i < list.length; i++) {
+  //     if (Date.UTC(list[i].shipBy) < pivotParam) {
+  //       lesser.push(list[i]);
+  //     } else {
+  //       greater.push(list[i]);
+  //     }
+  //   }
+  //   return this.quickSortByShipDate(lesser).concat(pivot, this.quickSortByShipDate(greater)).reverse();
+  // }
 
   private handleError(error: any): Promise<any> {
     console.error('An error has occurred', error)
