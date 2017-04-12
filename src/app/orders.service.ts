63f9912f66920f38cc7476a10c73103eb28b02ae
Ariana Clark <ariana.clark@gmail.com>
2017-04-11 21:28:30 -0600

    refined sorting algorithm

--- src/app/orders.service.ts
+++ src/app/orders.service.ts
@@ -15,7 +15,7 @@ export class OrdersService {
       .toPromise()
       .then(response => {
         console.log("response:", response.json())
-        let sorted: Array<Order> = response.json()[0].sort(this.compareDates)
+        let sorted: Array<Order> = this.quickSortByShipDate(response.json()[0])
         console.log(sorted)
         return sorted
       })
@@ -23,35 +23,21 @@ export class OrdersService {
 
   }
 
-  compareDates(a: Order, b: Order): number {
-    console.log(a, b)
-    let dateA: number = a.shipBy.valueOf()
-    let dateB: number = b.shipBy.valueOf()
-    if (dateA < dateB) {
-      return -1
+  quickSortByShipDate(list: Order[]): Order[] {
+    if (list.length == 0) {
+      return [];
     }
-    if (dateA > dateB) {
-      return 1
+    var lesser = []; var greater = []; var pivot = list[0];
+    var pivotParam = list[0].shipBy.valueOf();
+    for (var i = 1; i < list.length; i++) {
+      if (list[i].shipBy.valueOf() < pivotParam) {
+        lesser.push(list[i]);
+      } else {
+        greater.push(list[i]);
+      }
     }
-    return 0
-
+    return this.quickSortByShipDate(lesser).concat(pivot, this.quickSortByShipDate(greater));
   }
-
-  // quickSortByShipDate(list: any[]): any[] {
-  //   if (list.length == 0) {
-  //     return [];
-  //   }
-  //   var lesser = []; var greater = []; var pivot = list[0];
-  //   var pivotParam = Date.UTC(list[0].shipBy);
-  //   for (var i = 1; i < list.length; i++) {
-  //     if (Date.UTC(list[i].shipBy) < pivotParam) {
-  //       lesser.push(list[i]);
-  //     } else {
-  //       greater.push(list[i]);
-  //     }
-  //   }
-  //   return this.quickSortByShipDate(lesser).concat(pivot, this.quickSortByShipDate(greater)).reverse();
-  // }
 
   private handleError(error: any): Promise<any> {
     console.error('An error has occurred', error)
