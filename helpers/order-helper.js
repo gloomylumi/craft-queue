42494123e2361be48f8740f535ac35cc33534f80
Ari Clark <ari.clark@liqid.com>
2018-01-08 16:08:24 -0700

    Added default limit override to open receipts request. This fixes issue with older orders not being displayed.

--- helpers/order-helper.js
+++ helpers/order-helper.js
@@ -1,88 +1,88 @@
-'use strict'
-var exports = module.exports = {}
+'use strict';
+var exports = module.exports = {};
 const express = require( 'express' );
-const oa = require( './auth-helper' ).oa
-const querystring = require( 'querystring' )
-const Item = require( './order-helper' ).Item
+const oa = require( './auth-helper' ).oa;
+const querystring = require( 'querystring' );
+const Item = require( './order-helper' ).Item;
 
 
 
 
 exports.Order = class Order {
   constructor( receipt_id, creation_tsz, name, message_from_buyer, total_price ) {
-    this.orderId = receipt_id
-    this.orderDate = new Date( parseInt( creation_tsz + "000" ) )
-    this.buyerName = name
-    this.buyerMessage = message_from_buyer
-    this.totalPrice = total_price
-    this.items = []
+    this.orderId = receipt_id;
+    this.orderDate = new Date( parseInt( creation_tsz + "000" ) );
+    this.buyerName = name;
+    this.buyerMessage = message_from_buyer;
+    this.totalPrice = total_price;
+    this.items = [];
 
   }
 
   addItem( item ) {
-    this.items.push( item )
+    this.items.push( item );
   }
   calcShipBy() {
-    let maxProcessingBusinessDays = 0
+    let maxProcessingBusinessDays = 0;
     for ( var i = 0; i < this.items.length; i++ ) {
       if ( this.items[ i ].processingTime > maxProcessingBusinessDays ) {
-        maxProcessingBusinessDays = this.items[ i ].processingTime
+        maxProcessingBusinessDays = this.items[ i ].processingTime;
       }
     }
 
-    let maxProcessing = maxProcessingBusinessDays + ( 2 * Math.ceil( maxProcessingBusinessDays / 5 ) )
-    let shippingDate = new Date( this.orderDate.getFullYear(), this.orderDate.getMonth(), ( this.orderDate.getDate() + maxProcessing ) )
+    let maxProcessing = maxProcessingBusinessDays + ( 2 * Math.ceil( maxProcessingBusinessDays / 5 ) );
+    let shippingDate = new Date( this.orderDate.getFullYear(), this.orderDate.getMonth(), ( this.orderDate.getDate() + maxProcessing ) );
     if ( shippingDate.getDay() === 0 ) {
-      this.shipBy = new Date( this.orderDate.getFullYear(), this.orderDate.getMonth(), ( this.orderDate.getDate() + maxProcessing - 2 ) )
+      this.shipBy = new Date( this.orderDate.getFullYear(), this.orderDate.getMonth(), ( this.orderDate.getDate() + maxProcessing - 2 ) );
     } else if ( shippingDate.getDay() === 6 ) {
-      this.shipBy = new Date( this.orderDate.getFullYear(), this.orderDate.getMonth(), ( this.orderDate.getDate() + maxProcessing - 1 ) )
+      this.shipBy = new Date( this.orderDate.getFullYear(), this.orderDate.getMonth(), ( this.orderDate.getDate() + maxProcessing - 1 ) );
     } else {
-      this.shipBy = shippingDate
+      this.shipBy = shippingDate;
     }
   }
-}
+};
 
 exports.Item = class Item {
   constructor( transaction_id, listing_id, receipt_id, title, quantity, price, variations, url ) {
-    this.itemId = transaction_id
-    this.orderId = receipt_id
-    this.listingId = listing_id
-    this.title = title
-    this.quantity = quantity
-    this.price = price
-    this.transactionUrl = url
+    this.itemId = transaction_id;
+    this.orderId = receipt_id;
+    this.listingId = listing_id;
+    this.title = title;
+    this.quantity = quantity;
+    this.price = price;
+    this.transactionUrl = url;
     if ( !variations[ 0 ] ) {
-      this.variationName1 = null
-      this.variationValue1 = null
+      this.variationName1 = null;
+      this.variationValue1 = null;
     } else {
-      this.variationName1 = variations[ 0 ].formatted_name
-      this.variationValue1 = variations[ 0 ].formatted_value
+      this.variationName1 = variations[ 0 ].formatted_name;
+      this.variationValue1 = variations[ 0 ].formatted_value;
     }
     if ( !variations[ 1 ] ) {
-      this.variationName2 = null
-      this.variationValue2 = null
+      this.variationName2 = null;
+      this.variationValue2 = null;
     } else {
-      this.variationName2 = variations[ 1 ].formatted_name
-      this.variationValue2 = variations[ 1 ].formatted_value
+      this.variationName2 = variations[ 1 ].formatted_name;
+      this.variationValue2 = variations[ 1 ].formatted_value;
     }
-    this.imageThumbnailUrl = ''
-    this.imageFullUrl = ''
-    this.processingTime = 0
+    this.imageThumbnailUrl = '';
+    this.imageFullUrl = '';
+    this.processingTime = 0;
   }
 
-}
+};
 
 exports.timeoutQ = function timeoutQueue( array, etsyRequests, callback, callbackArray, min = 0, max = 4 ) {
   console.log( "recursion" );
-  let arrayChunk = array.slice( min, max )
+  let arrayChunk = array.slice( min, max );
   if ( min > array.length ) {
-    return callback( callbackArray )
+    return callback( callbackArray );
   } else {
-    etsyRequests( arrayChunk )
-    min += 5
-    max += 5
+    etsyRequests( arrayChunk );
+    min += 5;
+    max += 5;
     return setTimeout( function() {
-      timeoutQueue( array, etsyRequests, callback, callbackArray, min, max )
-    }, 600 )
+      timeoutQueue( array, etsyRequests, callback, callbackArray, min, max );
+    }, 600 );
   }
-}
+};
--- routes/orders.js
+++ routes/orders.js
@@ -1,32 +1,32 @@
-'use strict'
+'use strict';
 // Order Resource
 const express = require( 'express' );
 const oauth = require( 'oauth' );
 const knex = require( '../knex' );
 const router = express.Router();
-const querystring = require( 'querystring' )
-const Order = require( '../helpers/order-helper.js' ).Order
-const Item = require( '../helpers/order-helper.js' ).Item
-const timeoutQueue = require( '../helpers/order-helper.js' ).timeoutQ
-const oa = require( '../helpers/auth-helper.js' ).oa
+const querystring = require( 'querystring' );
+const Order = require( '../helpers/order-helper.js' ).Order;
+const Item = require( '../helpers/order-helper.js' ).Item;
+const timeoutQueue = require( '../helpers/order-helper.js' ).timeoutQ;
+const oa = require( '../helpers/auth-helper.js' ).oa;
 const camelizeKeys = require( 'humps' ).camelizeKeys;
 
 
 // BEGIN ORDER BUILDING
 router.get( '/', function( req, res, next ) {
-  const shop_id = req.session.shop_id
-  var orders = []
-  var listingIdArr = []
-  var listingsQuery = []
-  var listings = []
-  var imageListingIdArr = []
-  var uniqueImageListingIdArr = []
-  var uniqueListingIdArr = []
-  var images = []
-  var itemsTrxData = []
-  var itemsTrxLstData = []
-  var itemsComplete = []
-  var listingsAddedToDb = []
+  const shop_id = req.session.shop_id;
+  var orders = [];
+  var listingIdArr = [];
+  var listingsQuery = [];
+  var listings = [];
+  var imageListingIdArr = [];
+  var uniqueImageListingIdArr = [];
+  var uniqueListingIdArr = [];
+  var images = [];
+  var itemsTrxData = [];
+  var itemsTrxLstData = [];
+  var itemsComplete = [];
+  var listingsAddedToDb = [];
 
 
 
@@ -34,16 +34,17 @@ router.get( '/', function( req, res, next ) {
   new Promise( ( resolve, reject ) => {
       oa.getProtectedResource( //get transaction items
         "https://openapi.etsy.com/v2" + `/shops/${shop_id}/transactions?` + querystring.stringify( {
-          limit: 75
+          limit: 100
         } ),
         "GET",
         req.session.oauth.access_token,
         req.session.oauth.access_token_secret,
         function( error, data, response ) {
           if ( error ) {
-            return reject( error )
+            return reject( error );
           } else {
-            let rawItemsArr = JSON.parse( data ).results
+            let rawItemsArr = JSON.parse( data ).results;
+            console.log( 'transactions found:', rawItemsArr.length );
 
             rawItemsArr.forEach( function( transaction ) {
               // grab only unshipped orders
@@ -60,12 +61,12 @@ router.get( '/', function( req, res, next ) {
 
                 // using two arrays with implicit identical sequence
                 // push unique listing ids to array
-                let listingId = transaction.listing_id
+                let listingId = transaction.listing_id;
                 if ( uniqueListingIdArr.indexOf( listingId ) === -1 ) {
-                  uniqueListingIdArr.push( listingId )
+                  uniqueListingIdArr.push( listingId );
 
-                  let imageId = transaction.image_listing_id
-                  uniqueImageListingIdArr.push( [ listingId, imageId ] )
+                  let imageId = transaction.image_listing_id;
+                  uniqueImageListingIdArr.push( [ listingId, imageId ] );
                 }
 
                 // push unique image ids to array
@@ -75,12 +76,12 @@ router.get( '/', function( req, res, next ) {
                 // }
 
 
-                let item = new Item( transaction.transaction_id, transaction.listing_id, transaction.receipt_id, transaction.title, transaction.quantity, transaction.price, transaction.variations, transaction.url )
+                let item = new Item( transaction.transaction_id, transaction.listing_id, transaction.receipt_id, transaction.title, transaction.quantity, transaction.price, transaction.variations, transaction.url );
 
-                itemsTrxData.push( item )
+                itemsTrxData.push( item );
               }
 
-            } )
+            } );
 
 
             // query database for existing shop listing ids
@@ -89,14 +90,14 @@ router.get( '/', function( req, res, next ) {
               .where( 'shop_id', shop_id )
               .then( ( data ) => {
                 // pull listing ids from database into an array
-                let allDbListings = data.map( element => element.listing_id )
+                let allDbListings = data.map( element => element.listing_id );
                 // check if listings from current orders are already in the database and pull those listing ids into a separate array to be queried from the database
                 for ( var i = 0; i < uniqueListingIdArr.length; i++ ) {
                   if ( allDbListings.indexOf( uniqueListingIdArr[ i ] ) !== -1 ) {
-                    listingsQuery.push( uniqueListingIdArr.splice( i, 1 )[ 0 ] )
+                    listingsQuery.push( uniqueListingIdArr.splice( i, 1 )[ 0 ] );
                     // remove correspondign image id from unique array to maintain indentical implicit sequencing of arrays
-                    uniqueImageListingIdArr.splice( i, 1 )
-                    i--
+                    uniqueImageListingIdArr.splice( i, 1 );
+                    i--;
                   }
                 }
                 console.log( "uniqueListingIdArr:", uniqueListingIdArr );
@@ -112,85 +113,88 @@ router.get( '/', function( req, res, next ) {
                   .select()
                   .whereIn( 'listing_id', listingsQuery )
                   .then( ( data ) => {
-                    var dbListings = data.map( element => camelizeKeys( element ) )
+                    var dbListings = data.map( element => camelizeKeys( element ) );
                     // cross-reference listings with items and splice the items into the complete items array
-                    addDbListingsToItems( dbListings, itemsTrxData, itemsComplete )
+                    addDbListingsToItems( dbListings, itemsTrxData, itemsComplete );
                   } )
-                  .catch( err => next( err ) )
-              } )
-            resolve( itemsTrxData )
+                  .catch( err => next( err ) );
+              } );
+            resolve( itemsTrxData );
           }
         }
-      )
+      );
     } )
     .then( ( data ) => {
       return new Promise( ( resolve, reject ) => {
           oa.getProtectedResource( //get open receipts
-            "https://openapi.etsy.com/v2" + `/shops/${shop_id}/receipts/open`,
+            "https://openapi.etsy.com/v2" + `/shops/${shop_id}/receipts/open?`+ querystring.stringify( {
+              limit: 100
+            } ),
             "GET",
             req.session.oauth.access_token,
             req.session.oauth.access_token_secret,
             function( error, data, response ) {
               if ( error ) {
                 console.log( error );
-                return reject( error )
+                return reject( error );
               } else {
                 JSON.parse( data ).results.forEach( function( receipt ) {
-                  let order = new Order( receipt.receipt_id, receipt.creation_tsz, receipt.name, receipt.message_from_buyer, receipt.total_price )
-                  orders.push( order )
-                } )
-                resolve( orders )
+                  let order = new Order( receipt.receipt_id, receipt.creation_tsz, receipt.name, receipt.message_from_buyer, receipt.total_price );
+                  orders.push( order );
+                } );
+                console.log( 'orders found:', orders.length );
+                resolve( orders );
               }
             }
-          )
+          );
         } )
         .then( ( data ) => {
 
           ( function() {
             if ( uniqueListingIdArr.length !== 0 ) {
-              timeoutQueue( uniqueListingIdArr, etsyListingRequests, getImages, listings )
+              timeoutQueue( uniqueListingIdArr, etsyListingRequests, getImages, listings );
 
 
               function getImages( array ) {
-                timeoutQueue( uniqueImageListingIdArr, etsyImageListingRequests, addListingDataToItems, listings )
+                timeoutQueue( uniqueImageListingIdArr, etsyImageListingRequests, addListingDataToItems, listings );
               }
 
               function addListingDataToItems( listings, index = 0 ) {
                 if ( itemsTrxData.length === 0 ) {
-                  return addImageDatatoItems( images )
+                  return addImageDatatoItems( images );
                 }
                 if ( index > listings.length - 1 ) {
-                  itemsTrxLstData.push( itemsTrxData.shift() )
-                  return addListingDataToItems( listings, index )
+                  itemsTrxLstData.push( itemsTrxData.shift() );
+                  return addListingDataToItems( listings, index );
                 }
                 for ( var i = 0; i < itemsTrxData.length; i++ ) {
                   if ( itemsTrxData[ i ].listingId === listings[ index ].listing_id ) {
-                    itemsTrxData[ i ].processingTime = listings[ index ].processing_max
-                    itemsTrxLstData.push( itemsTrxData.splice( i, 1 )[ 0 ] )
-                    i--
+                    itemsTrxData[ i ].processingTime = listings[ index ].processing_max;
+                    itemsTrxLstData.push( itemsTrxData.splice( i, 1 )[ 0 ] );
+                    i--;
                   }
                 }
-                index++
-                return addListingDataToItems( listings, index )
+                index++;
+                return addListingDataToItems( listings, index );
 
               }
 
               function addImageDatatoItems( images, index = 0 ) {
                 if ( itemsTrxLstData.length === 0 ) {
-                  return addItemsToOrders()
+                  return addItemsToOrders();
                 }
                 if ( index > images.length - 1 ) {
-                  itemsComplete.push( itemsTrxLstData.shift() )
-                  return addListingDataToItems( listings, index )
+                  itemsComplete.push( itemsTrxLstData.shift() );
+                  return addListingDataToItems( listings, index );
                 }
                 for ( var i = 0; i < itemsTrxLstData.length; i++ ) {
                   if ( itemsTrxLstData[ i ].listingId === images[ index ].listing_id ) {
-                    itemsTrxLstData[ i ].imageThumbnailUrl = images[ index ].url_170x135
-                    itemsTrxLstData[ i ].imageFullUrl = images[ index ].url_fullxfull
+                    itemsTrxLstData[ i ].imageThumbnailUrl = images[ index ].url_170x135;
+                    itemsTrxLstData[ i ].imageFullUrl = images[ index ].url_fullxfull;
                     // check if the listing has been added to database
                     if ( listingsAddedToDb.indexOf( itemsTrxLstData[ i ].listingId ) === -1 ) {
                       // insert listing data into database
-                      let listingId = itemsTrxLstData[ i ].listingId
+                      let listingId = itemsTrxLstData[ i ].listingId;
                       knex( 'listings' )
                         .insert( {
                           listing_id: itemsTrxLstData[ i ].listingId,
@@ -200,59 +204,59 @@ router.get( '/', function( req, res, next ) {
                           image_full_url: itemsTrxLstData[ i ].imageFullUrl
                         } )
                         .then( ( data ) => {
-                          listingsAddedToDb.push( listingId )
+                          listingsAddedToDb.push( listingId );
                         } )
-                        .catch( ( err ) => next( err ) )
+                        .catch( ( err ) => next( err ) );
                     }
 
 
-                    itemsComplete.push( itemsTrxLstData.splice( i, 1 )[ 0 ] )
-                    i--
+                    itemsComplete.push( itemsTrxLstData.splice( i, 1 )[ 0 ] );
+                    i--;
                   }
                 }
-                index++
-                return addImageDatatoItems( images, index )
+                index++;
+                return addImageDatatoItems( images, index );
               }
             } else {
-              addItemsToOrders()
+              addItemsToOrders();
             }
 
 
             function addItemsToOrders( index = 0 ) {
               if ( itemsComplete.length === 0 ) {
                 orders.forEach( function( element ) {
-                  element.calcShipBy()
+                  element.calcShipBy();
                   //TODO: delete time variable and console logs
-                  let time = element.orderDate
-                  console.log( "name:", element.buyerName, "order time:", time.toString(), "timezone offset:", time.getTimezoneOffset() / 60 );
-                } )
+                  // let time = element.orderDate;
+                  // console.log( "name:", element.buyerName, "order time:", time.toString(), "timezone offset:", time.getTimezoneOffset() / 60 );
+                } );
 
-                res.send( [ orders, itemsComplete ] )
-                return
+                res.send( orders );
+                return;
               }
               if ( index > orders.length - 1 ) {
                 orders.forEach( function( element ) {
-                  element.calcShipBy()
+                  element.calcShipBy();
 
-                } )
+                } );
 
-                res.send( orders )
-                return
+                res.send( orders );
+                return;
               }
               for ( var i = 0; i < itemsComplete.length; i++ ) {
                 if ( itemsComplete[ i ].orderId === orders[ index ].orderId ) {
-                  orders[ index ].addItem( itemsComplete[ i ] )
-                  itemsComplete.splice( i, 1 )
-                  i--
+                  orders[ index ].addItem( itemsComplete[ i ] );
+                  itemsComplete.splice( i, 1 );
+                  i--;
                 }
               }
-              index++
-              return addItemsToOrders( index )
+              index++;
+              return addItemsToOrders( index );
             }
 
             function etsyListingRequests( arrayChunk = arrayChunk ) {
               arrayChunk.forEach( function( element ) {
-                let listingId = element
+                let listingId = element;
                 new Promise( ( resolve, reject ) => {
                   oa.getProtectedResource(
                     "https://openapi.etsy.com/v2" + `/listings/${listingId}`,
@@ -262,21 +266,21 @@ router.get( '/', function( req, res, next ) {
                     function( error, data, response ) {
                       if ( error ) {
                         console.log( error );
-                        return reject( error )
+                        return reject( error );
                       } else {
-                        listings.push( JSON.parse( data ).results[ 0 ] )
-                        return resolve( listings )
+                        listings.push( JSON.parse( data ).results[ 0 ] );
+                        return resolve( listings );
                       }
-                    } )
-                } )
-              } )
+                    } );
+                } );
+              } );
             }
 
             function etsyImageListingRequests( arrayChunk = arrayChunk ) {
 
               arrayChunk.forEach( function( ids ) {
-                let listingId = ids[ 0 ]
-                let imageId = ids[ 1 ]
+                let listingId = ids[ 0 ];
+                let imageId = ids[ 1 ];
                 new Promise( ( resolve, reject ) => {
                   oa.getProtectedResource(
                     "https://openapi.etsy.com/v2" + `/listings/${listingId}/images/${imageId}`,
@@ -287,46 +291,46 @@ router.get( '/', function( req, res, next ) {
 
                       if ( error ) {
                         console.log( "etsyImageListingRequests:", error );
-                        return reject( error )
+                        return reject( error );
                       } else {
-                        images.push( JSON.parse( data ).results[ 0 ] )
+                        images.push( JSON.parse( data ).results[ 0 ] );
 
-                        return resolve( images )
+                        return resolve( images );
 
                       }
-                    } )
-                } )
-              } )
+                    } );
+                } );
+              } );
             }
 
 
-          } )()
-        } )
-    } )
-} )
+          } )();
+        } );
+    } );
+} );
 
 function addDbListingsToItems( dbListings, itemsTrxData, itemsComplete, index = 0 ) {
   if ( index === dbListings.length ) {
-    return
+    return;
   }
   for ( var i = 0; i < itemsTrxData.length; i++ ) {
     if ( itemsTrxData[ i ].listingId === dbListings[ index ].listingId ) {
       // add listing data to item
-      itemsTrxData[ i ].processingTime = dbListings[ index ].processingTime
-      itemsTrxData[ i ].imageThumbnailUrl = dbListings[ index ].imageThumbnailUrl
-      itemsTrxData[ i ].imageFullUrl = dbListings[ index ].imageFullUrl
+      itemsTrxData[ i ].processingTime = dbListings[ index ].processingTime;
+      itemsTrxData[ i ].imageThumbnailUrl = dbListings[ index ].imageThumbnailUrl;
+      itemsTrxData[ i ].imageFullUrl = dbListings[ index ].imageFullUrl;
       // move item to complete item array
-      itemsComplete.push( itemsTrxData.splice( i, 1 )[ 0 ] )
+      itemsComplete.push( itemsTrxData.splice( i, 1 )[ 0 ] );
 
-      i--
+      i--;
     }
   }
 
-  return addDbListingsToItems( dbListings, itemsTrxData, itemsComplete, ( index + 1 ) )
+  return addDbListingsToItems( dbListings, itemsTrxData, itemsComplete, ( index + 1 ) );
 }
 
 
 
 
 
-module.exports = router
+module.exports = router;
